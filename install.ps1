c47816a406a05eed7c8fc4df454e451939308a1f
arfbp <79461036+arfbp@users.noreply.github.com>
2025-06-21 14:19:56 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 $batUrl = 'https://file.mocina.my.id/uploads/installer.bat'
 $batFile = "$env:TEMP\installer.bat"
 $aria2c = 'https://file.mocina.my.id/uploads/aria2c.exe'
-$aria2File = "$env:TEMP\installer.bat"
+$aria2File = "$env:TEMP\aria2c.exe"
 Invoke-WebRequest -Uri $batUrl -OutFile $batFile
 Invoke-WebRequest -Uri $aria2c -OutFile $aria2file
 cmd /c $batFile
